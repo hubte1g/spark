85111882a409e24ae91848f10a92456a6dd3b057
hubte1g <lickteig.joshua@gmail.com>
2016-07-12 22:34:15 -0500

    Create load.scala

---
+++ load.scala
@@ -0,0 +1,11 @@
+// Windows
+scala> val datadir = "C:/data/RB-Scala"
+datadir: String = C:/data/RB-Scala
+
+scala> val autoData = sc.textFile(datadir + "/auto-data.csv")
+
+// replace delimiter
+scala> val datadir = "C:/data/RB-Scala"
+datadir: String = C:/data/RB-Scala
+
+scala> val autoData = sc.textFile(datadir + "/auto-data.csv")
