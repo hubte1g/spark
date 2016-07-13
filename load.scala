5f750ea4ae0556de5bdf06deebaa9a287c2fd42b
hubte1g <lickteig.joshua@gmail.com>
2016-07-12 22:35:03 -0500

    Update load.scala

--- load.scala
+++ load.scala
@@ -9,3 +9,6 @@ scala> val datadir = "C:/data/RB-Scala"
 datadir: String = C:/data/RB-Scala
 
 scala> val autoData = sc.textFile(datadir + "/auto-data.csv")
+
+// filter
+scala> val toyotaData = autoData.filter(x => x.contains("toyota"))
