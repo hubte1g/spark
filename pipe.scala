b1d3f6d35a57da15b50f071bc18496a225ea23d6
hubte1g <lickteig.joshua@gmail.com>
2019-02-18 11:15:57 -0800

    Create pipe.scala

---
+++ pipe.scala
@@ -0,0 +1,9 @@
+// spark
+val conf = new SparkConf().setAppName("Filter Script")
+    val sc = new SparkContext(conf)
+
+    val dataRDD = sc.textFile("<Replace it with HDFS_CONF_DIR>" + "/weather_yagotyn.csv")
+
+    dataRDD.pipe("sed s/\"//g")
+      .pipe(Seq("awk", "-F;", "/^[0-9]/ && $22 < 1 {print $1, $2, $3, $6, $22}")).
+      collect().foreach(println)
