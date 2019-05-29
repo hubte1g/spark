78447d03d74750f37f7f3bf8d31f21faf1f493c3
hubte1g <lickteig.joshua@gmail.com>
2019-05-29 11:25:33 -0700

    Update app.scala

--- app.scala
+++ app.scala
@@ -2,4 +2,19 @@ object App {
  def main():
 }
 
-class NewClass(env:    String, conf: Config, logger: Logger)
+// ----------------
+
+package _._._
+
+import java.util.Calendar
+
+//import typesafe.config.Config
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Column, DataFrame, SaveMode, SparkSession}
+
+class NewClass(env:    String, conf: Config, logger: Logger) {
+ 
+ }
